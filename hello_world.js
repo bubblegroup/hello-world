a0a67545d359be87821ed8703099d42378db1010
Nickhil Sethi <ns1635@nyu.edu>
2018-01-11 12:51:09 -0500

    var port = 80;

--- hello_world.js
+++ hello_world.js
@@ -1,12 +1,12 @@
 express = require('express')
 
 var message = 'Hello World!';
+var port = 80;
 
 var server_app = express();
 server_app.get('/', function(req, res){
     res.end(message);
 });
 
-port = 80
 server_app.listen(port);
 console.log("Hello World app listening on port " + String(port));
