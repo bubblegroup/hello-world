1cffaee5efd390a159d7eca10bf8dc629bd6fb65
Nickhil Sethi <ns1635@nyu.edu>
2018-01-11 12:44:21 -0500

    hello world port fix

--- hello_world.js
+++ hello_world.js
@@ -7,5 +7,6 @@ server_app.get('/', function(req, res){
     res.end(message);
 });
 
-server_app.listen(80);
-console.log("Hello World app listening on port 80");
+port = 80
+server_app.listen(port);
+console.log("Hello World app listening on port #{port}");
