26d103f41e3e56be20437070205221742c81e9c5
Nickhil Sethi <ns1635@nyu.edu>
2018-01-14 18:05:20 -0500

    port 8080

--- hello_world.js
+++ hello_world.js
@@ -1,7 +1,11 @@
-express = require('express')
+/*
+	A simple hello world server used for testing purposes. 
+	Listens on a prespecified port and ends any incoming get request with Hello World
+*/
 
-var message = 'Hello World!';
-var port = 80;
+express = require('express')
+var message = 'Hello World!\n';
+var port = 8080;
 
 var server_app = express();
 server_app.get('/', function(req, res){
