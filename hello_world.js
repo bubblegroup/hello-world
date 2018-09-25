94261a7b243abfa78d2924965bc45d3427949336
Nickhil Sethi <ns1635@nyu.edu>
2018-09-25 17:22:35 -0400

    reporting id

--- hello_world.js
+++ hello_world.js
@@ -3,12 +3,19 @@
 	Listens on a prespecified port and ends any incoming get request with Hello World
 */
 
+request = require('request')
 express = require('express')
 var message = "Hello World!\n";
 var port = 8080;
 
 var server_app = express();
 exports.server = server_app;
+
+var id = ''
+server_app.use(function(req, res, next) {
+	res.setHeader("x-hello-world-id", id)
+	next()
+})
 
 server_app.get('/', function(req, res){
 	res.end(message);
@@ -20,7 +27,14 @@ server_app.get('/monitor', function(req, res){
 })
 
 if (require.main === module){
-	server_app.listen(port);
-	console.log("Hello World app listening on port " + String(port));	
+	request.get("http://169.254.169.254/latest/meta-data/instance-id", function(err, response, body){
+		if (err) {
+			throw err
+		} else {
+			id = body.toString()
+			server_app.listen(port)
+			console.log("Hello World app listening on port " + String(port))
+		}
+	})
 }
 
